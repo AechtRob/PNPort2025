d55f6eb7c443b474ba5d01ada4a5b3b2e1702775
AechtRob <Aecht_Rob@outlook.com>
2025-05-08 08:41:28 +0100

    Initial commit

--- src/main/java/com/github/aechtrob/prehistoricnature/block/blockbase/SeedHarverstable.java
+++
@@ -1,7 +0,0 @@
-package com.github.aechtrob.prehistoricnature.block.blockbase;
-
-public abstract class SeedHarverstable {
-
-    public abstract void getSeeds();
-    public abstract void placeSeeds();
-}
