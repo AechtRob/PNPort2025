e9577fcba0cfe69eccf678340ef1e498f4d81420
AechtRob <Aecht_Rob@outlook.com>
2025-05-11 20:56:06 +0100

    Initial commit

--- src/main/java/com/github/aechtrob/prehistoricnature/entity/entity/BenchSittableEntity.java
+++ src/main/java/com/github/aechtrob/prehistoricnature/entity/entity/BenchSittableEntity.java
@@ -32,7 +32,7 @@ public class BenchSittableEntity extends Entity {
 
     @Override
     public double getPassengersRidingOffset() {
-        return -0.1;
+        return 0.15;
     }
 
     @Override
