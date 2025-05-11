50d9c2f0c62648132d03a5789b9e3c3714b48fce
AechtRob <Aecht_Rob@outlook.com>
2025-05-11 20:33:34 +0100

    Initial commit

--- src/main/java/com/github/aechtrob/prehistoricnature/block/blockbase/PNBaseTrimmableBlock.java
+++ src/main/java/com/github/aechtrob/prehistoricnature/block/blockbase/PNBaseTrimmableBlock.java
@@ -10,6 +10,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
@@ -22,6 +23,11 @@ public abstract class PNBaseTrimmableBlock extends BaseEntityBlock {
 
     public PNBaseTrimmableBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState p_49232_) {
+        return RenderShape.MODEL;
     }
 
     @Override
