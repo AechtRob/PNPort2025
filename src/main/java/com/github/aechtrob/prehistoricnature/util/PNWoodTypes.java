4b907813c9b21d83eff18e3ad933ba79ad41c00b
Floi1 <hamnsar@gmail.com>
2025-06-27 14:24:26 +0100

    Gangampteris

--- src/main/java/com/github/aechtrob/prehistoricnature/util/PNWoodTypes.java
+++ src/main/java/com/github/aechtrob/prehistoricnature/util/PNWoodTypes.java
@@ -5,6 +5,7 @@ import net.minecraft.world.level.block.state.properties.BlockSetType;
 import net.minecraft.world.level.block.state.properties.WoodType;
 
 public class PNWoodTypes {
+    public static final WoodType GANGAMOPTERIS = WoodType.register(new WoodType(PrehistoricNature.MODID + "gangamopteris", BlockSetType.OAK));
     public static final WoodType GLOSSOPTERISA = WoodType.register(new WoodType(PrehistoricNature.MODID + "glossopteris_angustifolia", BlockSetType.OAK));
     public static final WoodType LEPIDODENDRON = WoodType.register(new WoodType(PrehistoricNature.MODID + "lepidodendron", BlockSetType.OAK));
     
