2e90203079f147a499c18fb3bee83f4cd19d8001
AechtRob <Aecht_Rob@outlook.com>
2025-07-11 14:11:11 +0100

    v. 1.21.5 Fossil Blocks and items

--- src/main/java/com/github/aechtrob/prehistoricnature/item/itembase/PNGeologicPick.java
+++ src/main/java/com/github/aechtrob/prehistoricnature/item/itembase/PNGeologicPick.java
@@ -1,6 +1,10 @@
 package com.github.aechtrob.prehistoricnature.item.itembase;
 
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
 
 public class PNGeologicPick extends Item {
 
@@ -8,4 +12,23 @@ public class PNGeologicPick extends Item {
         super(properties);
     }
 
+    @Override
+    public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
+        return true;
+    }
+
+    @Override
+    public boolean supportsEnchantment(ItemStack stack, Holder<Enchantment> enchantment) {
+        if (enchantment.is(ResourceLocation.fromNamespaceAndPath("minecraft","fortune"))
+                || enchantment.is(ResourceLocation.fromNamespaceAndPath("minecraft","mending"))
+                || enchantment.is(ResourceLocation.fromNamespaceAndPath("minecraft","efficiency"))
+                || enchantment.is(ResourceLocation.fromNamespaceAndPath("minecraft","durability"))
+                || enchantment.is(ResourceLocation.fromNamespaceAndPath("minecraft","unbreaking"))
+                || enchantment.is(ResourceLocation.fromNamespaceAndPath("minecraft","binding_curse"))
+                || enchantment.is(ResourceLocation.fromNamespaceAndPath("minecraft","vanishing_curse"))
+        ) {
+            return true;
+        }
+        return false;
+    }
 }
